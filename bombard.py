a595f11633aaf2a0fdd8058e4ca2c9e746caa604
Dmitrii Fedorov <dmitrii.fedorov@sentry.io>
2023-04-05 12:41:23 -0700

    test: Test zone outage
    Configs that allow to start the following setup:
    * 3-node zk cluster
    * 3-node storage cluster
    * 1-node query cluster
    
    schema file contains create command for simplified errors table
    bombard.py simple insert query generator

---
+++ bombard.py
@@ -0,0 +1,12 @@
+import requests
+import time
+
+PROJECT_ID: int = 1
+DELETED: int = 0
+RETENTION_DAYS: int = 32
+
+query: str = f"insert into errors_dist values ({PROJECT_ID}, now(), {DELETED}, generateUUIDv4(), {RETENTION_DAYS})"
+
+while True:
+    print(requests.post(f"http://localhost:8123/?query={query}"))
+    time.sleep(0.1)
