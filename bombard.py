a83649d4547acb3e1d0b95b03f58dd3110a14105
Dmitrii Fedorov <dmitrii.fedorov@sentry.io>
2023-04-20 06:53:32 -0700

    changes

--- bombard.py
+++ bombard.py
@@ -5,8 +5,10 @@ PROJECT_ID: int = 1
 DELETED: int = 0
 RETENTION_DAYS: int = 32
 
-query: str = f"insert into errors_dist values ({PROJECT_ID}, now(), {DELETED}, generateUUIDv4(), {RETENTION_DAYS})"
+insert: str = f"insert into errors_local values ({PROJECT_ID}, now(), {DELETED}, generateUUIDv4(), {RETENTION_DAYS})"
+count: str = f"select count(*) from errors_local"
 
 while True:
-    print(requests.post(f"http://localhost:8123/?query={query}"))
+    print(requests.post(f"http://localhost:8013/?query={insert}"))
+    print(requests.get(f"http://localhost:8013/?query={count}").text)
     time.sleep(1.0)
